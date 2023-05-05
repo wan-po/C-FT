008d13b1cc43cdd4e9aecff5143ea3a4f91575a0
PLUSPAY\boriswan <boriswan@pluspay.com.tw>
2023-05-05 16:43:28 +0800

    'push'

---
+++ AAT/TypeOfAndNameOf/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeOfAndNameOf
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine($"TypeOf:{typeof(Person)}，NameOf:{nameof(person)}");
+            Console.WriteLine($"TypeOf:{typeof(string)}");
+            //Typeof會將namespace也印出，nameof就是單純的名字而已
+            Console.ReadLine();
+        }
+    }
+}
