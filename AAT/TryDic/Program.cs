201728c0a77aa48273893283185fab77c0d33483
PLUSPAY\boriswan <boriswan@pluspay.com.tw>
2023-05-05 17:17:58 +0800

    push2

---
+++ AAT/TryDic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryDic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> myDic = new Dictionary<int,string>();
+            myDic.Add(0, "Test1");
+            myDic.Add(1, "Test2");
+            myDic.Add(2, "Test3");
+            myDic.Add(3, "Test4");
+
+            Console.WriteLine(myDic[1]);
+
+            int myDicC = myDic.Count;
+
+            for (int i = 0; i < myDicC; i++)
+            {
+                Console.WriteLine(myDic[i]);
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
