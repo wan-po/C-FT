5f7cc3e710d95cc823f0185cd62f2e892960127f
PLUSPAY\boriswan <boriswan@pluspay.com.tw>
2023-05-05 16:25:51 +0800

    'first'

---
+++ AAT/AAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myGuid
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            { 
+                string guid = Guid.NewGuid().ToString(); //微軟提供的唯一識別亂數
+                Console.WriteLine(guid);
+            }
+            Console.ReadLine();
+        }
+    }
+}
