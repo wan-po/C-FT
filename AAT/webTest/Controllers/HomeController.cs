b10b475d5b29a57251acde9c07348011a2adeea3
PLUSPAY\boriswan <boriswan@pluspay.com.tw>
2023-05-25 11:20:52 +0800

    'swagger'

---
+++ AAT/webTest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+
+        [HttpPost]
+        public string Test(int input)
+        {
+            string message ;
+            switch (input)
+            {
+                case 1:
+                    message ="you inputted 1"; 
+                    break;
+                case 2:
+                    message = "you inputted 2";
+                    break;
+                default:
+                    message = "default";
+                    break;
+            }
+            return message;
+            
+        }
+    }
+}
