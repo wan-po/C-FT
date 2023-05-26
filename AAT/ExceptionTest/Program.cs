2409a79c90f9660c3e0c3184d4018f44adbd1f2e
PLUSPAY\boriswan <boriswan@pluspay.com.tw>
2023-05-26 10:48:08 +0800

    ExceptionTest

---
+++ AAT/ExceptionTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2 };
+            GetValueFromArray(a, 5);
+            Console.ReadLine();
+           
+        }
+        static int GetValueFromArray(int[] array, int index)
+        {
+            try
+            {
+                return array[index];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+               
+                throw new ArgumentOutOfRangeException(
+                    "Parameter index is out of range.", e);
+            }
+        }
+    }
+}
